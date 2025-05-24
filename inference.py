2133379d6b715e3af468313c4726e294702790bf
ronnie <ronnieedavattathu15@gmail.com>
2025-05-24 03:05:09 +0200

    first commit to inference

---
+++ inference.py
@@ -0,0 +1,126 @@
+from typing import Optional
+import torch
+import time
+from pathlib import Path
+import json
+from sentencepiece import SentencePieceProcessor
+from tqdm import tqdm
+
+
+from model import Transformer, ModelArgs
+
+
+class LlaMa:
+
+    def __init__(self, model: Transformer, tokenizer: SentencePieceProcessor, model_args: ModelArgs):
+        super().__init__()
+        self.model = model
+        self.tokenizer = tokenizer
+        self.args = model_args
+
+    @staticmethod
+    def build(checkpoints_dir: str, tokenizer_path: str, load_model: bool, max_seq_len: int, max_batch_size: int, device: str):
+        prev_time = time.time()
+        if load_model: # load the saved model and it's trained weights/ parameters
+            checkpoints = sorted(Path(checkpoints_dir).glob("*.pth"))
+            assert len(checkpoints) > 0, f"No checkpoint files found in the {checkpoints_dir}"
+            ckpt_path = checkpoints[0]
+            print(f"Loading checkpoint {ckpt_path}")
+            checkpoint = torch.load(ckpt_path, map_location="cpu")
+            print(f"Loaded checkpoint in {time.time() - prev_time}")
+            prev_time = time.time()
+        with open(Path(checkpoints_dir) / "params.json", "r") as f:
+            params = json.loads(f.read())
+
+        model_args: ModelArgs = ModelArgs(
+            max_seq_len=max_seq_len,
+            max_batch_size=max_batch_size,
+            device=device,
+            **params
+        )
+
+        tokenizer = SentencePieceProcessor()
+        tokenizer.load(tokenizer_path)
+        model_args.vocab_size = tokenizer.vocab_size()
+        # How many total vocabularies do you have after doing the tokenization step
+
+        if device == "cuda":
+            torch.set_default_tensor_type(torch.cuda.HalfTensor)
+        else:
+            torch.set_default_tensor_type(torch.BFloat16Tensor)
+        
+        model = Transformer(model_args).to(device)
+
+        if load_model:
+            # The only unmatched key in the checkpoint is rope.freq remove it!
+            del checkpoint['rope.freqs']
+            model.load_state_dict(checkpoint, strict=True)
+            print(f"Loaded state dict in {time.time() - prev_time:.2f}s")
+        
+        return LlaMa(model, tokenizer, model_args)
+    
+    def text_completion(self, prompts: List[str], temperature: float = 0.6, top_p: float = 0.9, max_gen_len: Optional[int] = None):
+        # if the temperature value is huge, then you pull down the model's confidence about it's prediction
+        # if the temperature value is less, then you kind of boost the model's confidence about it's prediction
+
+        # Convert each prompt into token ids
+        prompt_tokens = [self.tokenizer.encode(prompt, out_type=int, add_bos=True, add_eos=False) for prompt in prompts]
+        # Make sure the batch size is not too large
+        batch_size = len(prompt_tokens)
+        assert batch_size <= self.args.max_batch_size, f"Batch size must be less than or equal to {self.args.max_batch_size}"
+        max_prompt_len = max(len(prompt) for prompt in prompts)
+        # Make sure that any of the prompt length is greater than the mentioned max prompt length
+        assert max_prompt_len <= self.args.max_seq_len, f"Prompt length must be less than or equal to {self.args.max_seq_len}"
+        total_len = min(self.args.max_seq_len, max_gen_len + max_prompt_len)
+
+        # Create the list that will contain the generated tokens, along with the initial prompt tokens
+        pad_id = self.tokenizer.pad_id()   # usually the pad_token related id is "0"
+        tokens = torch.full((batch_size, total_len), pad_id, dtype=torch.long, device=device)
+        for k, t in enumerate(prompt_tokens):
+            # Populate the initial tokens with the prompt tokens
+            tokens[k, : len(t)] = torch.tensor(t, dtype=torch.long, device=device)
+
+        eos_reached = torch.tensor([False] * batch_size, device=device)
+        prompt_tokens_mask = tokens != pad_id # True if the token is a prompt token, False otherwise
+        cur_iterator = tqdm(range(1, total_len), desc="Generating tokens")
+        for cur_pos in cur_iterator:
+            with torch.no_grad():
+                logits = self.model.forward(tokens[:, cur_pos-1:cur_pos], cur_pos)
+            if temperature > 0:
+                probs = torch.softmax(logits[:, -1] / temperature, dim=-1)
+                next_token = self._sample_top_p(probs, top_p)
+            else:
+                # Greedily select the token with the max probability
+                next_token = torch.argmax(logits[:, -1], dim=-1)
+            
+            next_token = next_token.reshape(-1)
+            # Only replace token if it's a padding token
+            next_token = torch.where(prompt_tokens_mask[:, cur_pos], tokens[:, cur_pos], next_token)
+            tokens[:, cur_pos] = next_token
+            # EOS is reached only if we found an EOS token in the place of the padding position
+            eos_reached |= (~prompt_tokens_mask[:, cur_pos]) & (next_token == self.tokenizer.eos_id)
+            if all(eos_reached):
+                break
+        
+        out_tokens = []  # let's visualize random token ids -> [101, 500, 566, 10009, etc]
+        out_text = []    # the corresponding text for the above token ids
+        for prompt_index, current_prompt_tokens in enumerate(tokens.tolist()):
+            # Cut to the EOS token if present
+            if self.tokenizer.eos_id in current_prompt_tokens:
+                # the above statement check if the current prompt tokens inside that of  a prompt does it have an "eos" token id?
+
+                # Now we are computing the position index of the EOS token and NOT IT'S TOKEN ID
+                eos_idx = current_prompt_tokens.index(self.tokenizer.eos_id)
+                current_prompt_tokens = current_prompt_tokens[:eos_idx]
+            out_tokens.append(current_prompt_tokens)
+            out_text.append(self.tokenizer.decode(current_prompt_tokens))
+        return (out_tokens, out_text)
+    
+
+    def _sample_top_p(self, probs, p):
+        # (B, vocab_size)
+        probs_sort, probs_idx = torch.sort(probs, dim=-1, descending=True)
+        # (B, vocab_size)
+        probs_sum = torch.cumsum(probs_sort, dim=-1)
+        # (B, vocab_size)
+        # 
